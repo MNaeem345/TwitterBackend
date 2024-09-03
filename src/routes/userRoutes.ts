14f58b4e8682e9c5e409cd60f673beb2ca00ef5b
Mohammed Naeem <mohammednaeem345@gmail.com>
2024-09-03 17:01:56 -0400

    prisma schema & user crud operations

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -43,15 +43,28 @@ router.get('/:id', async (req, res) => {
 })
 
 //update user
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     const {id} = req.params;
+    const {bio, name, image} = req.body;
+
+    try{
+        const result = await prisma.user.update({
+            where: {id: Number(id)},
+            data:{bio, name, image}
+        })
+        res.json(result)
+    } catch(e) {
+        res.status(400).json({error: `Failed to update the user`})
+    }
+
     res.status(501).json({error:`Not Implemented: ${id}`})
 })
 
 //delete user
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const {id} = req.params;
-    res.status(501).json({error:`Not Implemented: ${id}`})
+    await prisma.user.delete({where: {id: Number(id) } })
+    res.sendStatus(200)
 })
 
 
